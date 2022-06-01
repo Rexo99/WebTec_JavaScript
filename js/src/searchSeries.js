752e04f5b71071e90302c1bc579b52bf400bc3eb
marvinpieper <piepermarvin54@googlemail.com>
2022-06-01 16:21:32 +0200

    edit search

--- js/src/searchSeries.js
+++ js/src/searchSeries.js
@@ -2,8 +2,12 @@
 
 function serachSerie(user, serienname){
     url = user + '/searchByName/' + serienname;
+    serie = document.getElementById("serienname").value;
     fetch(url, {
         headers: { // ich weiß leider nicht mehr was wir im header angeben müssen, damit wir eine json datei zurcü bekommen
+
+        },
+        body: {
 
         },
         method: 'post'
