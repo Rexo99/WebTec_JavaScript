ad2d23a4ef11d3caa214d46bc6767a869afc4873
docx04 <luca-seliger@web.de>
2022-06-02 13:09:23 +0200

    validation in createSerie

--- js/src/createSeries.js
+++ js/src/createSeries.js
@@ -1,11 +1,12 @@
+/*
+ * creates a series with given inputs.
+ */
 function createSeries(){
     let title = document.getElementById("seriesTitle").value;
     let genre = document.getElementById("inputGenre").value;
     let plattform = document.getElementById("inputPlattform").value;
-    let seasons = 1;
-    try {
-        seasons = parseInt(document.getElementById("seriesSeasons").value);
-    } catch {
+    let seasons = document.getElementById("seriesSeasons").value;
+    if (isNaN(seasons)) {
         alert("Staffeln muss eine zahl sein!");
         return;
     }
@@ -15,7 +16,7 @@ function createSeries(){
         "streamedBy": plattform,
         "title": title
     }
-    let url = "http://localhost:8080/steam/api/serien/Luca/create_Series"
+    let url = "http://localhost:8080/steam/api/serien/Luca/create_Series";
 
     fetch(url,{
             method: 'post',
@@ -29,4 +30,23 @@ function createSeries(){
             window.location.assign("home.html");
         })
         .catch(err => console.error(err));
+}
+
+/*
+ * checks if all inputs are valid.
+ */
+function checkInput(){
+    document.getElementById("createButton").disabled = false;
+    document.getElementById("errorText").innerHTML = "";
+    let seasons = document.getElementById("seriesSeasons").value;
+    if (isNaN(seasons)) {
+        document.getElementById("createButton").disabled = true;
+        document.getElementById("errorText").innerHTML += "Staffeln muss eine Zahl sein.<br>"
+    } 
+
+    let title = document.getElementById("seriesTitle").value;
+    if(!title){ //checks if title is null or empty (truthy / falsy)
+        document.getElementById("createButton").disabled = true;
+        document.getElementById("errorText").innerHTML += "Bitte einen Titel eingeben."
+    }
 }
