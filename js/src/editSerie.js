cb1076da4fd68b7c382da947ee33a8f53a99dff8
marvinpieper <piepermarvin54@googlemail.com>
2022-06-05 19:19:49 +0200

    edit button is working now. The only think is, that you cant change the title of the serie. Because the function in the background will identify the serie with the title. If you change the title the function will create a new Serie with the new title

--- js/src/editSerie.js
+++ js/src/editSerie.js
@@ -7,11 +7,12 @@ function editSerie(){
     let numberOfSession = document.getElementById("inputNumberOfSeasonsSingleSerie").value;
     let streamedBy = document.getElementById("inputPlattformSingleSerie").value;
 
+
     // get username from seesionStorage
     let user = window.sessionStorage.getItem("username");
 
     // create the url for the REST request
-    let url = "localhost:8080/steam/api/serien/" + user + "/modify_Series";
+    let url = "http://localhost:8080/steam/api/serien/" + user + "/modify_Series";
 
     // create request body with a json code
     let modify = {"genre" : genre
@@ -20,27 +21,23 @@ function editSerie(){
         ,"title": title
     };
 
+
     // REST request
     fetch(url, {
         method: 'post',
         headers: {
             "Accept":"application/json",
             "Content-Type":"application/json"
-            // "Access-Control-Allow-Origin": "*"
         },
         body:
             JSON.stringify(modify)
     })
-        .then(response => response.json())
+        .then(response => response.text())
         .then(data => {
-            if(data === null){
-                window.alert("No Data")
-            }
             window.location.assign("home.html");
-            console.log(data);
         })
         .catch(
-            err => document.getElementById("answer").innerHTML = 'Fetch error ' + err
+            err => console.log(err)
         );
 }
 
@@ -57,21 +54,18 @@ function fillSiteWithData(){
     let nOS = window.localStorage.getItem("nOS");
 
     // Fill site with Serie Data
-    //document.getElementById("inputTitleSingleSerie").innerHTML = title.toString();
     document.getElementById("inputTitleSingleSerie").setAttribute('value', title.toString());
     document.getElementById("inputGenreSingleSerie").setAttribute('value', genre.toString());
     document.getElementById("inputNumberOfSeasonsSingleSerie").setAttribute('value', nOS.toString());
     document.getElementById("inputPlattformSingleSerie").setAttribute('value', streamedBy.toString());
 
-    if(title === null){
-        console.log("title is null");
-    }else {
-        console.log(title);
-        console.log(genre);
-        console.log(streamedBy);
-        console.log(nOS);
-    }
+    // check in the console if the correct serie will be edit
+    console.log(title);
+    console.log(genre);
+    console.log(streamedBy);
+    console.log(nOS);
 
+    // delete local storage
     window.localStorage.clear();
 }
 
